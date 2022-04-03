0de1546d31329f4c5b1dedf611c8b604cbed9e54
莱奥斯 <621862169@qq.com>
2022-04-03 17:52:30 +0800

    Fix some problem

---
+++ src/main/kotlin/com/faithl/abbie/controller/blog/ArticleController.kt
@@ -0,0 +1,94 @@
+package com.faithl.abbie.controller.blog
+
+import cn.dev33.satoken.annotation.SaCheckLogin
+import cn.dev33.satoken.annotation.SaCheckPermission
+import com.faithl.abbie.entity.blog.Article
+import com.faithl.abbie.entity.user.User
+import com.faithl.abbie.model.blog.ArticleModel
+import com.faithl.abbie.util.Security
+import com.faithl.abbie.util.gson
+import com.faithl.abbie.util.respondJson
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
+
+/**
+ * @author Leosouthey
+ * @since 2022/4/2-23:34
+ **/
+@RestController
+@RequestMapping("/api/v1/article")
+@CrossOrigin(originPatterns = ["http://localhost:3000"])
+class ArticleController {
+
+    @GetMapping("/list")
+    fun list(): List<ArticleModel> {
+        return transaction {
+            Article.all().map {
+                it.toArticleModel()
+            }
+        }
+    }
+
+    @PostMapping("/create")
+    @SaCheckLogin
+    @SaCheckPermission("article-create")
+    fun create(@RequestBody article: ArticleModel): String {
+        return transaction {
+            val result = Article.new {
+                title = article.title
+                content = article.content
+                author = User.findById(Security.loggedId())!!.id
+                createdAt = LocalDateTime.now()
+                updatedAt = LocalDateTime.now()
+            }
+            respondJson(HttpStatus.OK, "create success") {
+                add("article", gson.toJsonTree(result.toArticleModel()))
+            }
+        }
+    }
+
+    @GetMapping("/detail/{id}")
+    fun detail(@PathVariable id: Int): ArticleModel? {
+        return transaction { Article.findById(id)?.toArticleModel() }
+    }
+
+    @PutMapping("/update/{id}")
+    @SaCheckLogin
+    @SaCheckPermission("article-update")
+    fun update(@PathVariable id: Int, @RequestBody article: ArticleModel): String {
+        val result = transaction { Article.findById(id) }
+        return if (result == null) {
+            respondJson(HttpStatus.NOT_FOUND, "update failed") {
+                addProperty("reason", "article not found")
+            }
+        } else {
+            transaction {
+                result.title = article.title
+                result.content = article.content
+                result.author = User.findById(Security.loggedId())!!.id
+                result.updatedAt = LocalDateTime.now()
+            }
+            respondJson(HttpStatus.OK, "update success") {
+                add("article", gson.toJsonTree(result.toArticleModel()))
+            }
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @SaCheckLogin
+    @SaCheckPermission("article-delete")
+    fun delete(@PathVariable id: Int): String {
+        val result = transaction { Article.findById(id) }
+        return if (result == null) {
+            respondJson(HttpStatus.NOT_FOUND, "delete failed") {
+                addProperty("reason", "article not found")
+            }
+        } else {
+            transaction { result.delete() }
+            respondJson(HttpStatus.OK, "delete success")
+        }
+    }
+
+}
---
+++ src/main/kotlin/com/faithl/abbie/controller/blog/CommentController.kt
@@ -0,0 +1,95 @@
+package com.faithl.abbie.controller.blog
+
+import cn.dev33.satoken.annotation.SaCheckLogin
+import com.faithl.abbie.entity.blog.Article
+import com.faithl.abbie.entity.blog.Comment
+import com.faithl.abbie.entity.blog.Comments
+import com.faithl.abbie.entity.user.User
+import com.faithl.abbie.model.blog.CommentModel
+import com.faithl.abbie.util.Security
+import com.faithl.abbie.util.gson
+import com.faithl.abbie.util.respondJson
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
+
+/**
+ * @author Leosouthey
+ * @since 2022/4/3-1:04
+ **/
+@RestController
+@RequestMapping("/api/v1/comment")
+@CrossOrigin(originPatterns = ["http://localhost:3000"])
+class CommentController {
+
+    @RequestMapping("/create/{articleId}")
+    @SaCheckLogin
+    fun create(@PathVariable articleId: Int, @RequestBody comment: CommentModel): String {
+        val article = transaction { Article.findById(articleId) }
+        val insertedComment = transaction {
+            Comment.new {
+                this.article = article!!.id
+                this.content = comment.content
+                this.author = User.findById(Security.loggedId())!!.id
+                if (comment.parentId != null) {
+                    this.parentId = Comment.findById(comment.parentId)!!.id
+                }
+                this.createdAt = LocalDateTime.now()
+                this.updatedAt = LocalDateTime.now()
+            }
+        }
+        return respondJson(HttpStatus.OK, "create success") {
+            add("comment", gson.toJsonTree(insertedComment.toCommentModel()))
+        }
+    }
+
+    @RequestMapping("/delete/{commentId}")
+    @SaCheckLogin
+    fun delete(@PathVariable commentId: Int): String {
+        val comment = Comment.findById(commentId)
+        return if (comment != null) {
+            if (comment.author.value == Security.loggedId()) {
+                transaction {
+                    comment.delete()
+                }
+                respondJson(HttpStatus.OK, "delete success")
+            } else {
+                respondJson(HttpStatus.FORBIDDEN, "you are not author")
+            }
+        } else {
+            respondJson(HttpStatus.NOT_FOUND, "comment not found")
+        }
+    }
+
+    @RequestMapping("/update/{commentId}")
+    @SaCheckLogin
+    fun update(@PathVariable commentId: Int, content: String): String {
+        val comment = Comment.findById(commentId)
+        return if (comment != null) {
+            transaction {
+                comment.content = content
+                comment.updatedAt = LocalDateTime.now()
+            }
+            respondJson(HttpStatus.OK, "update success")
+        } else {
+            respondJson(HttpStatus.NOT_FOUND, "comment not found")
+        }
+    }
+
+    @RequestMapping("/list/{articleId}")
+    fun list(@PathVariable articleId: Int) {
+        val article = Article.findById(articleId)
+        if (article != null) {
+            val comments = transaction {
+                Comment.find { Comments.article eq articleId }.toList()
+            }
+            respondJson(HttpStatus.OK, "list success") {
+                add("comments", gson.toJsonTree(comments.map { it.toCommentModel() }))
+            }
+        } else {
+            respondJson(HttpStatus.NOT_FOUND, "article not found")
+        }
+    }
+
+}
